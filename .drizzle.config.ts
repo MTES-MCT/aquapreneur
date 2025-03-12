29f37e29beddd9dc5463f04c89b4f9411a5b6a8b
Cedric Rossi <cedric@i9n.fr>
2025-03-12 18:41:17 +0100

    Mise en place du système de migrations de Drizzle

--- .drizzle.config.ts
+++ .drizzle.config.ts
@@ -3,14 +3,18 @@ import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
+  dialect: 'postgresql',
   schema: './db/schema',
-
+  out: './migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL
   },
-
+  migrations: {
+    table: 'migrations',
+    schema: 'public'
+  },
   verbose: true,
   strict: true,
-  dialect: 'postgresql',
-  casing: 'snake_case'
+  casing: 'snake_case',
+  breakpoints: false
 });
