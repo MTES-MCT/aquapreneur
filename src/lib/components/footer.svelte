95aefc89b47dfec90b158a239026194cd695a493
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-03-25 15:53:10 +0100

    Ajout du pied de page

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,86 @@
+<footer class="fr-footer" role="contentinfo" id="footer-7475">
+  <div class="fr-container">
+    <div class="fr-footer__body">
+      <div class="fr-footer__brand fr-enlarge-link">
+        <p class="fr-logo">
+          République
+          <br />Française
+        </p>
+      </div>
+      <div class="fr-footer__content">
+        <p class="fr-footer__content-desc">
+          Aquapreneur est porté par la Fabrique Numérique de l’Écologie (MTE-MCT), et sponsorisé par
+          le Ministère de l’Agriculture et de la Souveraineté alimentaire et par la Direction
+          générale des Affaires maritimes, de la Pêche et de l’Aquaculture du Ministère de la
+          Transition écologique.
+        </p>
+        <ul class="fr-footer__content-list">
+          <li class="fr-footer__content-item">
+            <a
+              target="_blank"
+              rel="noopener external"
+              title="info.gouv.fr - nouvelle fenêtre"
+              id="footer__content-link-7362"
+              class="fr-footer__content-link"
+              href="https://info.gouv.fr">info.gouv.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              target="_blank"
+              rel="noopener external"
+              title="service-public.fr - nouvelle fenêtre"
+              id="footer__content-link-7363"
+              class="fr-footer__content-link"
+              href="https://service-public.fr">service-public.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              target="_blank"
+              rel="noopener external"
+              title="legifrance.gouv.fr - nouvelle fenêtre"
+              id="footer__content-link-7364"
+              class="fr-footer__content-link"
+              href="https://legifrance.gouv.fr">legifrance.gouv.fr</a
+            >
+          </li>
+          <li class="fr-footer__content-item">
+            <a
+              target="_blank"
+              rel="noopener external"
+              title="data.gouv.fr - nouvelle fenêtre"
+              id="footer__content-link-7365"
+              class="fr-footer__content-link"
+              href="https://data.gouv.fr">data.gouv.fr</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="fr-footer__bottom">
+      <!-- <ul class="fr-footer__bottom-list">
+        <li class="fr-footer__bottom-item">
+          <a class="fr-footer__bottom-link" href="#">Accessibilité : non conforme</a>
+        </li>
+        <li class="fr-footer__bottom-item">
+          <a class="fr-footer__bottom-link" href="#">Mentions légales</a>
+        </li>
+        <li class="fr-footer__bottom-item">
+          <a class="fr-footer__bottom-link" href="#">Données personnelles</a>
+        </li>
+      </ul> -->
+      <div class="fr-footer__bottom-copy">
+        <p>
+          Sauf mention explicite de propriété intellectuelle détenue par des tiers, les contenus de
+          ce site sont proposés sous <a
+            href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+            target="_blank"
+            rel="noopener external"
+            title="Licence etalab - nouvelle fenêtre">licence etalab-2.0</a
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
