5faabc7f07519dfe874fd7f8cdc57328b87eb7ab
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-03-25 18:28:06 +0100

    Ajout des mentions légales

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+  import ExternalLink from '$lib/components/external-link.svelte';
+</script>
+
+<div class="fr-grid-row">
+  <div class="fr-col-8">
+    <h1>Mentions légales</h1>
+
+    <p class="fr-text--xs">25 mars 2025</p>
+
+    <h2>Éditeur de la plateforme</h2>
+
+    <p>
+      La plateforme « Aquapreneur » est éditée par la Fabrique numérique et la Direction générale
+      des affaires maritimes, de la pêche et de l’aquaculture (DGAMPA) du ministère de la Transition
+      écologique et de la Cohésion des territoires, située :
+    </p>
+    <p>
+      1 place Carpeaux <br />
+      92800 Puteaux<br />
+      France
+    </p>
+
+    <h2>Directeur de la publication</h2>
+
+    <p>Éric BANEL, Directeur général des affaires maritimes, de la pêche et de l’aquaculture.</p>
+
+    <h2>Hébergement de la plateforme</h2>
+
+    <p>Cette plateforme est hébergée par :</p>
+    <p>
+      Scalingo<br />
+      13 rue Jacques Peirotes <br />
+      67000 Strasbourg<br />
+      France
+    </p>
+
+    <h2>Accessibilité</h2>
+
+    <p>
+      La conformité aux normes d’accessibilité numérique est un objectif ultérieur mais nous tâchons
+      de rendre ce site accessible à toutes et à tous.
+    </p>
+
+    <h3>Signaler un dysfonctionnement</h3>
+
+    <p>
+      Si vous rencontrez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une
+      fonctionnalité du site, merci de nous en faire part : <a href="mailto:aquapreneur@mer.gouv.fr"
+        >aquapreneur@mer.gouv.fr</a
+      >.
+    </p>
+    <p>
+      Si vous n’obtenez pas de réponse rapide de notre part, vous êtes en droit de faire parvenir
+      vos doléances ou une demande de saisine au Défenseur des droits.
+    </p>
+
+    <h3>En savoir plus</h3>
+
+    <p>
+      Pour en savoir plus sur la politique d’accessibilité numérique de l’État :<br />
+      <ExternalLink
+        href="https://accessibilite.numerique.gouv.fr"
+        title="accessibilite.numerique.gouv.fr"
+        >https://accessibilite.numerique.gouv.fr</ExternalLink
+      >
+    </p>
+
+    <h2>Sécurité</h2>
+
+    <p>
+      Le site est protégé par un certificat électronique, matérialisé pour la grande majorité des
+      navigateurs par un cadenas. Cette protection participe à la confidentialité des échanges. En
+      aucun cas les services associés à la plateforme ne seront à l’origine d’envoi de courriels
+      pour demander la saisie d’informations personnelles.
+    </p>
+  </div>
+</div>
