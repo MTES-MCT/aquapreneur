2153d983c20393ca342aea795d0d0b517f7c32cc
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-03-26 11:17:27 +0100

    Ajout CGU et politique de confidentialité

---
+++ src/routes/politique-de-confidentialite/+page.svelte
@@ -0,0 +1,407 @@
+<script>
+  import ExternalLink from '$lib/components/external-link.svelte';
+</script>
+
+<div class="fr-grid-row">
+  <div class="fr-col-8">
+    <h1>Politique de confidentialité</h1>
+    <p class="fr-text--xs">25 mars 2025</p>
+
+    <p>
+      Le responsable de traitement s’engage à ce que la collecte et le traitement de vos données
+      personnelles, effectués à partir du site soient conformes au règlement européen n° 2016/679
+      général sur la protection des données du 27 avril 2016 dit « RGPD » et à la loi n° 78-17
+      relative à l’informatique, aux fichiers et aux libertés dite « LIL ».
+    </p>
+
+    <p>Aquapreneur est un service numérique qui permet de :</p>
+
+    <ul>
+      <li>
+        Permettre aux titulaires d’une concession d’exploitation aquacole de télédéclarer leurs
+        productions dans les conditions prévues par les textes réglementaires ;
+      </li>
+      <li>
+        Mettre à disposition des informations administratives et techniques publiques relatives aux
+        concessions des exploitations aquacoles ;
+      </li>
+      <li>Proposer une carte interactive des concessions d’exploitation aquacoles.</li>
+    </ul>
+
+    <h2>I - Responsable de traitement</h2>
+    <p>
+      Le responsable de traitement est la Direction générale des affaires maritimes, de la pêche et
+      de l’aquaculture (DGAMPA) du ministère de la Transition Écologique et de la Cohésion des
+      Territoires, représenté par son directeur général, Eric Banel.
+    </p>
+
+    <h2>II - Finalités</h2>
+
+    <p>
+      Aquapreneur est un service numérique qui traite des données à caractère personnel pour les
+      raisons suivantes :
+    </p>
+    <ul>
+      <li>Créer d’un compte et connexion au Service ;</li>
+      <li>Communiquer des données économiques sur une exploitation aquacole ;</li>
+      <li>Télédéclarer des productions aquacoles ;</li>
+      <li>
+        Soumettre une demande de concession aquacole (non disponible dans la phase
+        d’expérimentation) ;
+      </li>
+      <li>
+        Mettre en accès libre sur Internet des informations relatives aux exploitations aquacoles
+        (non disponible dans la phase d’expérimentation).
+      </li>
+    </ul>
+
+    <h2>III - Base légale</h2>
+
+    <p>Aquapreneur traite des données à caractère personnel en se basant sur :</p>
+    <ul>
+      <li>
+        L’exécution d’une mission d’intérêt public ou relevant de l’exercice de l’autorité publique
+        dont est investi le responsable de traitement au sens de l’article 6-1 e) du RPGD ;
+      </li>
+      <li>
+        Cette mission d’intérêt public découle des attributions confiées à la Direction générale des
+        affaires maritimes, de la pêche et de l’aquaculture (DGAMPA) par l’article 9 du décret n°
+        2008-680, l’article 9.1.1 de l’arrêté du 9 juillet 2008 portant organisation des
+        administrations centrales des ministères chargés de la transition écologique, de la cohésion
+        des territoires et de la mer et l’article R. 923-11 du code rural et de la pêche maritime.
+      </li>
+    </ul>
+
+    <h2>IV - Catégories de données</h2>
+
+    <p>Aquapreneur traite les données à caractère personnel suivantes :</p>
+
+    <ul>
+      <li>
+        <strong>Données de création de compte et de connexion</strong> : Nom, prénom, adresse mail
+      </li>
+      <li>
+        <strong>Données d’entreprise</strong> : nom, prénom, téléphone, courrier électronique ainsi
+        que le nom de l’entreprise, numéros de SIREN, numéro de TVA intracommunautaire et l’adresse
+        du siège social.
+      </li>
+      <li>
+        <strong>Données relatives aux demandes d’une concession aquacole</strong> : Non disponible
+        dans la phase d’expérimentation.
+      </li>
+      <li>
+        <strong>Données des concessions d’exploitation aquacole en accès libre</strong> : toutes les
+        informations nécessaires à l’établissement et la consultation d’une cartographie interactive
+        qu’il s’agisse ou non de données à caractère personnel et notamment, géolocalisation de la
+        parcelle cadastrale. Le nom de l’entreprise et l’identité du représentant légal ne sont pas
+        disponibles dans la phase d’expérimentation.
+      </li>
+    </ul>
+
+    <h2>V - Durée de conservation</h2>
+
+    <div
+      class="fr-table--sm fr-table fr-table--bordered fr-table--no-scroll"
+      id="table-sm-component"
+    >
+      <div class="fr-table__wrapper">
+        <div class="fr-table__container">
+          <div class="fr-table__content">
+            <table id="table-sm">
+              <thead>
+                <tr>
+                  <th scope="col"> Type de données </th>
+                  <th scope="col"> Durée de la conservation </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données de création de compte et de connexion </td>
+                  <td> 3 ans à compter de la dernière activité</td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données d’entreprise </td>
+                  <td> 3 ans à compter de la dernière activité</td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données relatives aux demandes d’une concession aquacole </td>
+                  <td> 3 ans à compter de la dernière activité </td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données des concessions d’exploitation aquacole </td>
+                  <td>
+                    2 ans en base active puis archivage intermédiaire jusqu’au terme de la durée de
+                    la concession avant archivage définitif (article R. 923-10 du code rural et de
+                    la pêche administrative prévoit que la durée maximum d’une concession est de 35
+                    ans)</td
+                  >
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Cookies </td>
+                  <td> 13 mois</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h2>VI - Vos droits</h2>
+
+    <p>Vous disposez :</p>
+
+    <ul>
+      <li>D’un droit d’information et d’un droit d’accès à vos données ;</li>
+      <li>D’un droit de rectification ;</li>
+      <li>D’un droit d’opposition ;</li>
+      <li>D’un droit à la limitation du traitement.</li>
+    </ul>
+    <p>
+      Pour les exercer, contactez-nous à : <a href="mailto:aquapreneur@mer.gouv.fr"
+        >aquapreneur@mer.gouv.fr</a
+      >.
+    </p>
+
+    <p>
+      Puisque ce sont des droits personnels, nous ne traiterons votre demande que si nous sommes en
+      mesure de vous identifier. Dans le cas où nous ne parvenons pas à vous identifier, nous
+      pouvons être amenés à vous demander une preuve de votre identité.
+    </p>
+    <p>
+      Pour vous aider dans votre démarche, vous trouverez un modèle de courrier élaboré par la CNIL
+      ici :
+      <ExternalLink
+        href="https://www.cnil.fr/fr/modele/courrier/exercer-son-droit-dacces"
+        title="Exercer son droit d’accès">exercer son droit d’accès</ExternalLink
+      >
+    </p>
+
+    <p>
+      Nous nous engageons à vous répondre dans un délai raisonnable qui ne saurait dépasser 1 mois à
+      compter de la réception de votre demande.
+    </p>
+
+    <h2>VII - Destinataires</h2>
+
+    <p>Les accès aux données sont strictement encadrés et juridiquement justifiés.</p>
+
+    <p>
+      Durant la phase d’expérimentation, seuls les agents publics de l’équipe Aquapreneur (DGAMPA et
+      SSP) ont accès aux données.
+    </p>
+    <p>Suite à l’expérimentation, les personnes suivantes vont avoir accès aux données :</p>
+
+    <div
+      class="fr-table--sm fr-table fr-table--bordered fr-table--no-scroll"
+      id="table-sm-component"
+    >
+      <div class="fr-table__wrapper">
+        <div class="fr-table__container">
+          <div class="fr-table__content">
+            <table id="table-sm">
+              <thead>
+                <tr>
+                  <th scope="col"> Données </th>
+                  <th scope="col"> Destinataires </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données de création de compte et de connexion </td>
+                  <td>
+                    Agents publics et personnels habilités des services déconcentrés de l’Etat</td
+                  >
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données d’entreprise </td>
+                  <td>
+                    Agents publics et personnels habilités des services déconcentrés de l’Etat<br />
+                    Les personnels habilités de l’Agence Française pour le développement et la promotion
+                    de l’agriculture biologique (Agence Bio)</td
+                  >
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données relatives aux demandes d’une concession aquacole </td>
+                  <td>
+                    Agents publics et personnels habilités des services déconcentrés de l’Etat
+                  </td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Données des concessions d’exploitation aquacole </td>
+                  <td>
+                    Agents publics et personnels habilités des services déconcentrés de l’Etat.</td
+                  >
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <p>
+      Les agents des organisations professionnels, des centres techniques, des comités d’animation
+      des filières pourront avoir accès à des données dans un cadre juridique qu’il reste à
+      déterminer.
+    </p>
+
+    <h2>VIII - Mesures de sécurité</h2>
+
+    <p>Nous mettons en place plusieurs mesures pour sécuriser les données :</p>
+    <ul>
+      <li>Stockage des données en base de données ;</li>
+      <li>Cloisonnement des données ;</li>
+      <li>Mesures de traçabilité ;</li>
+      <li>Surveillance ;</li>
+      <li>Protection contre les virus, malwares et logiciels espions ;</li>
+      <li>Protection des réseaux ;</li>
+      <li>Sauvegarde ;</li>
+      <li>Mesures restrictives limitant l’accès physique aux données à caractère personnel.</li>
+    </ul>
+
+    <h2>IX - Qui nous aide à traiter les données ?</h2>
+    <p>
+      Certaines des données sont envoyées à d’autres acteurs, appelés “sous-traitants de données”,
+      pour qu’ils nous aident à les manipuler. Nous nous assurons qu’ils respectent strictement le
+      RGPD et qu’ils apportent des garanties suffisantes en matière de sécurité.
+    </p>
+
+    <div
+      class="fr-table--sm fr-table fr-table--bordered fr-table--no-scroll"
+      id="table-sm-component"
+    >
+      <div class="fr-table__wrapper">
+        <div class="fr-table__container">
+          <div class="fr-table__content">
+            <table id="table-sm">
+              <thead>
+                <tr>
+                  <th scope="col"> Sous-traitant </th>
+                  <th scope="col"> Pays destinataire </th>
+                  <th scope="col"> Traitement réalisé </th>
+                  <th scope="col"> Garanties </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td>Au Carré </td>
+                  <td> France</td>
+                  <td> Administration de la plateforme</td>
+                  <td> </td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> ScaleWay</td>
+                  <td> France</td>
+                  <td> Hébergement</td>
+                  <td> </td>
+                </tr>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> Ksaar</td>
+                  <td> France</td>
+                  <td> Support</td>
+                  <td> </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h2>X - Cookies</h2>
+
+    <p>
+      Un cookie est un fichier déposé sur votre terminal lors de la visite d’un site. Il a pour but
+      de collecter des informations relatives à votre navigation et de vous adresser des services
+      adaptés à votre terminal (ordinateur, mobile ou tablette).
+    </p>
+
+    <p>
+      En application de l’article 5(3) de la directive 2002/58/CE modifiée concernant le traitement
+      des données à caractère personnel et la protection de la vie privée dans le secteur des
+      communications électroniques, transposée à l’article 82 de la loi n° 78-17 du 6 janvier 1978
+      relative à l’informatique, aux fichiers et aux libertés, les traceurs ou cookies suivent deux
+      régimes distincts.
+    </p>
+
+    <ul>
+      <li>
+        Les cookies strictement nécessaires au service ou ayant pour finalité exclusive de faciliter
+        la communication par voie électronique sont dispensés de consentement préalable au titre de
+        l’article 82 de la loi n° 78-17 du 6 janvier 1978.
+      </li>
+      <li>
+        Les cookies n’étant pas strictement nécessaires au service ou n’ayant pas pour finalité
+        exclusive de faciliter la communication par voie électronique doivent être consenti par
+        l’utilisateur.
+      </li>
+    </ul>
+    <p>
+      Ce consentement de la personne concernée pour une ou plusieurs finalités spécifiques constitue
+      une base légale au sens du RGPD et doit être entendu au sens de l’article 6-1 a) du RGPD et à
+      la libre circulation des données.
+    </p>
+
+    <div
+      class="fr-table--sm fr-table fr-table--bordered fr-table--no-scroll"
+      id="table-sm-component"
+    >
+      <div class="fr-table__wrapper">
+        <div class="fr-table__container">
+          <div class="fr-table__content">
+            <table id="table-sm">
+              <caption> Cookies recensés sur Aquapreneur </caption>
+              <thead>
+                <tr>
+                  <th scope="col"> Nom du cookie </th>
+                  <th scope="col"> Finalités </th>
+                  <th scope="col"> Pays hébergement </th>
+                  <th scope="col"> Garanties </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="table-sm-row-key-1" data-row-key="1">
+                  <td> _ga_BBLE8XKC73 </td>
+                  <td> Mesure d’audience pour la cartographie</td>
+                  <td> Etats-Unis </td>
+                  <td> Aucune donnée à caractère personnel</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <p>
+      À tout moment, vous pouvez refuser l’utilisation des cookies et désactiver le dépôt sur votre
+      ordinateur en utilisant la fonction dédiée de votre navigateur (fonction disponible notamment
+      sur Microsoft Internet Explorer 11, Google Chrome, Mozilla Firefox, Apple Safari et Opera).
+    </p>
+    <p>
+      Pour aller plus loin, vous pouvez consulter les fiches proposées par la Commission Nationale
+      de l’Informatique et des Libertés (CNIL) :
+    </p>
+    <ul>
+      <li>
+        <ExternalLink
+          href="https://www.cnil.fr/fr/cookies-et-autres-traceurs/regles/cookies/que-dit-la-loi"
+          title="Cookies & traceurs : que dit la loi ?"
+          >Cookies & traceurs : que dit la loi ?</ExternalLink
+        >
+      </li>
+
+      <li>
+        <ExternalLink
+          href="https://www.cnil.fr/fr/cookies-et-autres-traceurs/comment-se-proteger/maitriser-votre-navigateur"
+          title="Cookies : les outils pour les maîtriser"
+          >Cookies : les outils pour les maîtriser</ExternalLink
+        >
+      </li>
+    </ul>
+  </div>
+</div>
+
+<style>
+</style>
