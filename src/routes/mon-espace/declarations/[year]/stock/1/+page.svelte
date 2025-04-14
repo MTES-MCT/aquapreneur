cb72ba10e9bf481650da904338637449ff90c276
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-04-14 20:48:06 +0200

    Page stock préliminaire

---
+++ src/routes/mon-espace/declarations/[year]/stock/1/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import LeafPict from '@gouvfr/dsfr/dist/artwork/pictograms/environment/leaf.svg';
+
+  import NavigationLinks from '$lib/components/navigation-links.svelte';
+  import Pictogram from '$lib/components/pictogram.svelte';
+</script>
+
+<Pictogram pict={LeafPict} width={124} height={124}></Pictogram>
+
+<h1 class="fr-h2 fr-mt-6v">Nous allons maintenant faire le point sur votre stock.</h1>
+
+<p>
+  Cette étape concerne votre volume en stock à la fin de votre exercice comptable. Vous devrez
+  confirmer ou compléter les informations relative à votre stock selon l’espèce et le stade
+  d’élevage.
+</p>
+
+<NavigationLinks prevHref="../production/2" nextHref="2" />
---
+++ src/routes/mon-espace/declarations/[year]/stock/2/+page.ts
@@ -0,0 +1,8 @@
+export const load = async ({ parent }) => {
+  const { year } = await parent();
+
+  return {
+    title: `Déclaration ${year} • Stock • 2`,
+    step: 'stock'
+  };
+};
