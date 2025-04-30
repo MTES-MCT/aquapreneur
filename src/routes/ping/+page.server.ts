7c3694b42535ea1df29448f8ccb88ff79f5e3acb
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-04-30 13:12:29 +0200

    Le monitorage se fait maintenant sur une page HTML complète
    Pour faciliter l’intégration updown dans dashlord

--- src/routes/ping/+page.server.ts
+++ src/routes/ping/+page.server.ts
@@ -1,17 +1,20 @@
+import { error } from '@sveltejs/kit';
+
 import { db } from '$db';
 
 import { utilisateurs } from '$db/schema/auth';
 
 import * as logger from '$utils/logger';
 
-export const GET = async () => {
+export const load = async () => {
   // On fait une requête quelconque sur la BDD pour qu’une erreur soit déclenchée
   // si elle n’est pas disponible
   try {
     await db.select().from(utilisateurs).limit(1);
   } catch (err) {
     logger.exception(err, 'La base de données n’est pas accessible');
-    return new Response('La base de données n’est pas accessible', { status: 500 });
+    error(500, 'La base de données n’est pas accessible');
   }
-  return new Response('Aquapreneur OK');
+
+  return { title: 'Monitorage', statusMessage: 'Aquapreneur OK' };
 };
