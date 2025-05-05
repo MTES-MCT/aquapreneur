cbf2e190ae6bd9b06e09ceb0a5ccfb493014faa3
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-05-05 19:40:43 +0200

    Spécification des en-têtes de sécurité #10
    * CSP
    
    * Autres en-têtes

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
+import * as dotenvx from '@dotenvx/dotenvx';
+
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+dotenvx.config({ ignore: ['MISSING_ENV_FILE'], quiet: true });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +13,27 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    csp: {
+      mode: 'nonce',
+      directives: {
+        'base-uri': ['none'],
+        'default-src': ['none'],
+        'object-src': ['none'],
+        'script-src': [],
+        'style-src': ['none'],
+        'style-src-elem': ['self'],
+        'style-src-attr': ['unsafe-inline'],
+        'connect-src': ['self'],
+        'font-src': ['self'],
+        'frame-ancestors': ['none'],
+        'form-action': ['self'],
+        'img-src': ['self', 'data:'],
+        'require-trusted-types-for': ['script'],
+        'report-uri': [
+          process.env.SENTRY_CSP_REPORT_URI ? process.env.SENTRY_CSP_REPORT_URI : 'none'
+        ]
+      }
+    },
     alias: {
       $db: './db',
       $data: './data',
