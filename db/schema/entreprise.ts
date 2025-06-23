db45ba731cc314a66c2e1e00654f1ae1507515f8
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-06-23 15:22:04 +0200

    Suppression des géometries Atena

--- db/schema/entreprise.ts
+++ db/schema/entreprise.ts
@@ -1,20 +1,20 @@
-import { type InferSelectModel } from "drizzle-orm";
-import { pgTable, text } from "drizzle-orm/pg-core";
+// import { type InferSelectModel } from "drizzle-orm";
+// import { pgTable, text } from "drizzle-orm/pg-core";
 
-import { timestamps } from ".";
+// import { timestamps } from ".";
 
-export const entreprise = pgTable("entreprises", {
-	siren: text().primaryKey(),
-	...timestamps,
-	// TODO : remplir les données via SIRENE
-});
+// export const entreprise = pgTable("entreprises", {
+// 	siren: text().primaryKey(),
+// 	...timestamps,
+// 	// TODO : remplir les données via SIRENE
+// });
 
-export const etablissements = pgTable("etablissements", {
-	siret: text().primaryKey(),
-	siren: text().references(() => entreprise.siren),
-	// TODO : remplir les données via SIRENE
+// export const etablissements = pgTable("etablissements", {
+// 	siret: text().primaryKey(),
+// 	siren: text().references(() => entreprise.siren),
+// 	// TODO : remplir les données via SIRENE
 
-	...timestamps,
-});
+// 	...timestamps,
+// });
 
-export type Etablissement = InferSelectModel<typeof etablissements>;
+// export type Etablissement = InferSelectModel<typeof etablissements>;
