d14b645b299b11df777e52b6494b190ea569a72a
Cedric Rossi <cedric.rossi@beta.gouv.fr>
2025-07-23 17:24:55 +0200

    Amélioration de la gestion des permissions

--- src/routes/producteur/+layout.server.ts
+++ src/routes/producteur/+layout.server.ts
@@ -1,13 +1,10 @@
-import { redirect } from "@sveltejs/kit";
+import assert from "assert";
 
 import { ADMIN_CURRENT_SIRET_COOKIE_NAME } from "$lib/constants";
 
 export const load = async ({ parent, cookies }) => {
 	const { utilisateur } = await parent();
-
-	// Cette route n’est accessible qu’aux utilisateurs connectés, et validés
-	if (!utilisateur) redirect(307, "/");
-	if (!utilisateur.valide) redirect(307, "/validation");
+	assert(utilisateur && utilisateur.valide);
 
 	let siret: string | null = null;
 	if (utilisateur.estAdmin) {
